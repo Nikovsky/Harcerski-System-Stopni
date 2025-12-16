b556b1e28e88fe10c9724a8d49ca69c11fcf3737
nikovsky <nfeduniewicz@gmail.com>
2025-12-17 00:14:28 +0100

    chore: init monorepo + infra + ci

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,12 @@
+const js = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+
+module.exports = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-console": "off"
+    }
+  }
+];
