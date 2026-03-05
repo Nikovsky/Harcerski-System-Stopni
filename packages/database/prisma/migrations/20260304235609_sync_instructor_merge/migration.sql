bb9913079778344ebaa7215c4500625bfc942943
Nikovsky <nfeduniewicz@gmail.com>
2026-03-05 01:13:17 +0100

    DATABASE - Naprawa replay migracji sync_instructor_merge

--- packages/database/prisma/migrations/20260304235609_sync_instructor_merge/migration.sql
+++ packages/database/prisma/migrations/20260304235609_sync_instructor_merge/migration.sql
@@ -1,8 +1,8 @@
 -- DropIndex
-DROP INDEX "CommissionMeeting_date_createdAt_idx";
+DROP INDEX IF EXISTS "CommissionMeeting_date_createdAt_idx";
 
 -- DropIndex
-DROP INDEX "MeetingRegistration_active_meeting_candidate_unique";
+DROP INDEX IF EXISTS "MeetingRegistration_active_meeting_candidate_unique";
 
 -- DropIndex
-DROP INDEX "MeetingRegistration_active_slot_unique";
+DROP INDEX IF EXISTS "MeetingRegistration_active_slot_unique";
